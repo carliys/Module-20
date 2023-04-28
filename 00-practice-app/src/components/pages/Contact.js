4496c03682980ac0542d020745530433fe14555d
carliys <cflores1833@yahoo.com>
2023-04-27 18:21:45 -0700

    contact me

--- 00-practice-app/src/components/pages/Contact.js
+++ 00-practice-app/src/components/pages/Contact.js
@@ -5,17 +5,13 @@ export default function Contact() {
     <div>
       <h1>Contact Page</h1>
       <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+      If you guys have any questions about anything or about me, please feel free to contact me!
+      </p>
+      <p>
+        My Email is Carlosvargas1833@gmail.com
+      </p>
+      <p>
+        My GitHub is CARlIYS
       </p>
     </div>
   );
